79572da5484873cbdbd3222c53760a1247a5dd62
iiPython <ben@iipython.dev>
2025-07-30 15:20:37 -0500

    yeah fix the workflow

--- lrcup/__init__.py
+++ lrcup/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "iiPython"
 __license__ = "MIT"
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 from .controller import LRCLib  # noqa: F401
 from .audio import AudioFile, format_lyrics  # noqa: F401
