f51d71a2938e12b0ee4de34823fc94566a6cfc52
skj-skj <paljain4444@gmail.com>
2025-09-18 22:54:49 +0530

    bump up the version to 0.10.2

--- lrcup/__init__.py
+++ lrcup/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "iiPython"
 __license__ = "MIT"
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 from .controller import LRCLib  # noqa: F401
 from .audio import AudioFile, format_lyrics  # noqa: F401
